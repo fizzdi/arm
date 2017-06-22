2acaf02a446577af35f3d1187f9fb1ce0915c2ae
fizzdi <diku55@mail.ru>
2017-06-22 23:34:46 +0300

    rename button

--- ARM_medacc/report_frp.Designer.cs
+++ ARM_medacc/report_frp.Designer.cs
@@ -40,9 +40,11 @@
             this.groupBox1.Controls.Add(this.cb_frp);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(248, 119);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(331, 146);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "МОЛ:";
@@ -52,30 +54,33 @@
             this.cb_frp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cb_frp.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.cb_frp.FormattingEnabled = true;
-            this.cb_frp.Location = new System.Drawing.Point(7, 26);
+            this.cb_frp.Location = new System.Drawing.Point(9, 32);
+            this.cb_frp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cb_frp.Name = "cb_frp";
-            this.cb_frp.Size = new System.Drawing.Size(235, 29);
+            this.cb_frp.Size = new System.Drawing.Size(312, 35);
             this.cb_frp.TabIndex = 4;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(7, 61);
+            this.button1.Location = new System.Drawing.Point(9, 75);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(235, 49);
+            this.button1.Size = new System.Drawing.Size(313, 60);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Остатки";
+            this.button1.Text = "Сформировать";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // report_frp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(268, 141);
+            this.ClientSize = new System.Drawing.Size(357, 174);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "report_frp";
